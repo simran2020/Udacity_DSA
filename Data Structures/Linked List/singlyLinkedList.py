00ea8422c27da1d064101a5dc0fbad9259c16987
Bharat Poptwani <bharat.poptwani@bookmyshow.com>
2020-03-09 20:56:50 +0530

    Python implementation of Singly Linked List

---
+++ Data Structures/Linked List/singlyLinkedList.py
@@ -0,0 +1,90 @@
+class ListNode:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+    def __repr__(self):
+        return repr(self.data)
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        """
+        Return a string representation of the list.
+        Takes O(n) time.
+        """
+        nodes = []
+        curr = self.head
+        while curr:
+            nodes.append(repr(curr))
+            curr = curr.next
+        return '[' + ', '.join(nodes) + ']'
+
+    def prepend(self, data):
+        self.head = ListNode(data=data, next=self.head)
+
+    def append(self, data):
+        """
+        Insert a new element at the end of the list.
+        Takes O(n) time.
+        """
+        if not self.head:
+            self.head = ListNode(data=data)
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = ListNode(data=data)
+
+    def find(self, key):
+        curr = self.head
+        while curr and curr.data != key:
+            curr = curr.next
+        return curr
+
+    def remove(self, key):
+        curr = self.head
+        prev = None
+        while curr and curr.data != key:
+            prev = curr
+            curr = curr.next
+        # Unlink it from the list
+        if prev is None:
+            self.head = curr.next
+        elif curr:
+            prev.next = curr.next
+            curr.next = None
+
+    def reverse(self):
+        curr = self.head
+        prev_node = None
+        next_node = None
+        while curr:
+            next_node = curr.next
+            curr.next = prev_node
+            prev_node = curr
+            curr = next_node
+        self.head = prev_node
+
+
+lst = SinglyLinkedList()
+print(lst)
+
+lst.prepend(23)
+lst.prepend('a')
+lst.prepend(42)
+lst.prepend('X')
+lst.append('the')
+lst.append('end')
+print(lst)
+
+result = lst.find('X')
+print(result)
+result = lst.find('y')
+print(result)
+
+lst.reverse()
+print(lst)
